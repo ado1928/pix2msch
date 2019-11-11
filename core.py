a158b98ae4a8c183848848297659425de4586476
ado1928 <47899778+ado1928@users.noreply.github.com>
2019-11-11 19:57:07 +0100

    Add files via upload

---
+++ core.py
@@ -0,0 +1,110 @@
+import struct, zlib, os
+from PIL import Image
+
+tiles = []
+colorarray = [
+    217, 157, 115,
+    140, 127, 169,
+    235, 238, 245,
+    178, 198, 210,
+    247, 203, 164,
+    39, 39, 39,
+    141, 161, 227,
+    249, 163, 199,
+    119, 119, 119,
+    83, 86, 92,
+    203,217, 127,
+    244,186, 110,
+    243, 233, 121,
+    116, 87, 206,
+    255, 121, 94,
+    255, 170, 95
+    ]
+
+tuple_array = [tuple(colorarray[t*3:t*3+3]) for t in range(len(colorarray)//3)]
+palette = Image.new("P", (16, 16))
+palette.putpalette(colorarray*16)
+palette.load()
+
+def quantize(img, dither, transparency_treshold):
+    
+    img = img.convert("RGBA")
+    imgq = img.convert("RGB")
+    imgq = imgq._new(imgq.im.convert("P", 1 if dither else 0, palette.im))
+    
+    imgA = Image.new("RGBA", img.size)
+    pixels = imgA.load()
+    imgq = imgq.convert("RGB")
+    for y in range(img.size[1]):
+        for x in range(img.size[0]):
+            if img.getpixel((x, y))[3] > transparency_treshold:
+                pixels[x, y] = imgq.getpixel((x, y))
+            else:
+                pixels[x, y] = (0, 0, 0, 0)
+
+    print("Quantization complete")
+    
+    return (imgq, imgA)
+
+def imgtomsch(imgfile, name, save_location, dither, transparency_treshold):
+    
+    img = quantize(Image.open(imgfile), dither, transparency_treshold)[0].rotate(-90, expand=True)
+    imgA = quantize(Image.open(imgfile), dither, transparency_treshold)[1].rotate(-90, expand=True)
+    
+    width, height = img.size
+    for y in range(img.size[1]):
+        for x in range(img.size[0]):
+            if imgA.getpixel((x, y))[3] > transparency_treshold:
+                tiles.append((x, y, tuple_array.index(img.getpixel((x, y)))))
+
+    print("Converted pixels into an array of tiles")
+
+    class ByteBuffer():
+        def __init__(self, data=bytearray()):
+            self.data = data
+            
+        def writeShort(self, int):
+            self.data += struct.pack(">H", int)
+
+        def writeUTF(self, str):
+            self.writeShort(len(str))
+            self.data += bytes(str.encode("UTF"))
+            
+        def writeByte(self, int):
+            self.data += struct.pack("b", int)
+            
+        def writeInt(self, int):
+            self.data += struct.pack(">i", int)
+            
+    data = ByteBuffer()
+
+    data.writeShort(width)
+    data.writeShort(height)
+
+    data.writeByte(1)
+
+    data.writeUTF("name")
+    data.writeUTF(name)
+
+    data.writeByte(1)
+
+    data.writeUTF("sorter")
+    data.writeInt(len(tiles))
+
+    print("Header written")
+
+    for tile in tiles:
+        data.writeByte(0)
+        data.writeShort(tile[1])
+        data.writeShort(tile[0])
+        data.writeInt(tile[2])
+        data.writeByte(0)
+
+    print("Tile data written")
+
+    os.chdir(os.path.expandvars(save_location))
+    file = open(name + ".msch", "wb")
+    file.write(b"msch\x00"+zlib.compress(data.data))
+    file.close()
+
+    print("Successfully saved {0} ".format(name + ".msch"))
---
+++ gui.py
@@ -0,0 +1,138 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+import tkinter.font, os
+from PIL import Image, ImageTk
+import core
+
+class GUI():
+    def __init__(self, root):
+        self.root = root
+        root.title("pix2msch")
+        root.resizable(False, False)
+        root.geometry("600x500")
+
+        photo = PhotoImage(file = "background.png")
+        background = Label(root, image = photo)
+        background.image = photo
+        background.place(x = -2, y = -2)
+        
+        font = tkinter.font.Font(family="Consolas", size=12)
+        
+        self.dither = IntVar(value=1)
+        self.dither_c = Checkbutton(
+        root, font = font, text="Dithering",
+        bg = "#35373C", fg = "#B7BBCE",
+        activebackground="#515359", activeforeground="#cccccc", bd = 0,
+        selectcolor="#515359",
+        var = self.dither
+        )
+        self.dither_c.place(x = 300, y = 375)
+        
+        self.name = StringVar()
+        self.path = StringVar()
+        self.transparency = StringVar()
+        
+        name_entry = Entry(root, font = font, width = 25, bg = "#35373C", fg = "#B7BBCE", textvariable = self.name)
+        name_entry.place(x = 300, y = 278)
+        
+        path_entry = Entry(root, font = font, width = 25, bg = "#35373C", fg = "#B7BBCE", textvariable = self.path)
+        path_entry.place(x = 300, y = 308)
+        
+        transparency_entry = Entry(root, font = font, width = 25, bg = "#35373C", fg = "#B7BBCE", textvariable = self.transparency)
+        transparency_entry.place(x = 300, y = 338)
+        
+        name_entry.insert(0, "schematic")
+        path_entry.insert(0, "%appdata%\\Mindustry\\schematics")
+        transparency_entry.insert(0, "127")
+        
+        self.open_image_b = Button(root, font = font, command=self.open_image, text = "Open Image...", bg = "#35373C", fg = "#B7BBCE", activebackground="#515359", activeforeground="#cccccc", bd = 0)
+        self.open_image_b.place(x = 300, y = 240, anchor = CENTER)
+        
+        convert_b = Button(root, command=self.convert, font = font, text = "Convert to msch...", bg = "#35373C", fg = "#B7BBCE", activebackground="#515359", activeforeground="#cccccc", bd = 0)
+        convert_b.place(x = 350, y = 450, anchor = CENTER)
+        
+        preview_b = Button(root, command=self.preview, font = font, text = "Preview", bg = "#35373C", fg = "#B7BBCE", activebackground="#515359", activeforeground="#cccccc", bd = 0)
+        preview_b.place(x = 200, y = 450, anchor = CENTER)
+        
+        
+        
+    def open_image(self):
+        root.update()
+        self.file = filedialog.askopenfilename()
+        self.open_image_b.configure(text = self.file)
+        try:
+            self.image = Image.open(self.file)
+        except:
+            self.open_image_b.configure(text = "Invalid Image file!")
+            
+        root.update()
+        
+    def convert(self):
+        try:
+            int(self.transparency.get())
+            self.image
+            if int(self.transparency.get()) > 255:
+                messagebox.showerror("Error", "Transparency Treshold must not exceed 255")
+                raise Exception
+            elif int(self.transparency.get()) < 0:
+                messagebox.showerror("Error", "Transparency Treshold must not be negative")
+                raise Exception
+            if not(os.path.isdir(os.path.expandvars(self.path.get()))):
+                messagebox.showerror("Error", "Invalid path")
+                raise Exception
+            if self.name.get() == "":
+                messagebox.showerror("Error", "Please enter a name")
+                raise Exception
+        except AttributeError:
+            messagebox.showerror("Error", "No image selected")
+        except ValueError:
+            messagebox.showerror("Error", "Transparency Treshold must be a number")
+        else:
+            try:
+                core.imgtomsch(self.file, self.name.get(), self.path.get(), self.dither.get(), int(self.transparency.get()))
+            except Exception as e:
+                messagebox.showerror("oh no", e)
+            else:
+                messagebox.showinfo("Success", "Successfully converted image to msch")
+    
+    def preview(self):
+        
+        try:
+            self.window.destroy()
+        except:
+            pass
+        
+        try:
+            int(self.transparency.get())
+            self.image
+            if int(self.transparency.get()) > 255:
+                messagebox.showerror("Error", "Transparency Treshold must not exceed 255")
+                raise Exception
+            elif int(self.transparency.get()) < 0:
+                messagebox.showerror("Error", "Transparency Treshold must not be negative")
+                raise Exception
+        except AttributeError:
+            messagebox.showerror("Error", "No image selected")
+        except ValueError:
+            messagebox.showerror("Error", "Transparency Treshold must be a number")
+        else:
+            targetsize = 700
+            qimg = core.quantize(self.image, self.dither.get(), 127)[1]
+            
+            sizemultiplier = targetsize/max(qimg.size)
+            self.window = Toplevel(root)
+            self.window.geometry(str(int(qimg.size[0]*sizemultiplier)) + "x" + str(int(qimg.size[1]*sizemultiplier)))
+            
+            image = ImageTk.PhotoImage(qimg.resize((int(qimg.size[0]*sizemultiplier), int(qimg.size[1]*sizemultiplier))))
+            background = Label(self.window, image = image)
+            self.window.image = image
+            self.window.resizable(False, False)
+            background.place(x = -2, y = -2)
+
+root = Tk()
+try:
+    GUI(root)
+except Exception as e:
+    messagebox.showerror("oh no", e)
+root.mainloop()
